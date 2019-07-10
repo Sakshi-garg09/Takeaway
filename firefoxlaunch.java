feeb8ad7870ab44ac539f01e4aa7e91a5023d102
Sakshi-garg09 <52748210+Sakshi-garg09@users.noreply.github.com>
2019-07-10 17:15:47 +0200

    firefox file

---
+++ firefoxlaunch.java
@@ -0,0 +1,13 @@
+package Package;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class firefoxlaunch {
+
+	public static void main(String[] args) {
+	 WebDriver driver = new FirefoxDriver();
+
+	}
+
+}
